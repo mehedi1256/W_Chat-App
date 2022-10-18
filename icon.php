fab6eb51dcd5833ae1b9a88a02ca626f6423b660
mehedi1256 <mehedihassanshovospirit@gmail.com>
2022-10-18 10:35:43 +0600

    first commit

---
+++ icon.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Icon</title>
+
+</head>
+
+<body>
+    <button style="border-radius:5px;bottom:2vh;right:2vh;position:fixed;"><img src="send.svg"
+            alt="message_icon"></button>
+</body>
+
+</html>
